b37ca9191bd29434042a608d57545d2c2b5e9fb0
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-10-22 08:19:13 +0530

    spelling fix

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -15,28 +15,28 @@ import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathName = usePathname();
-  const isModile = useMediaQuery("(max-width: 768px)");
+  const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
   const [isResetting, setIsResetting] = useState(false);
-  const [isCollapsed, setIsCollapsed] = useState(isModile);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   useEffect(() => {
-    if (isModile) {
+    if (isMobile) {
       collapse();
     } else {
       resetWidth();
     }
-  }, [isModile]);
+  }, [isMobile]);
 
   useEffect(() => {
-    if (isModile) {
+    if (isMobile) {
       collapse();
     }
-  }, [pathName, isModile]);
+  }, [pathName, isMobile]);
 
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -77,12 +77,12 @@ export const Navigation = () => {
       setIsCollapsed(false);
       setIsResetting(true);
 
-      sidebarRef.current.style.width = isModile ? "100%" : "240px";
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
       navbarRef.current.style.setProperty(
         "width",
-        isModile ? "0" : "calc(100% - 240px)"
+        isMobile ? "0" : "calc(100% - 240px)"
       );
-      navbarRef.current.style.setProperty("left", isModile ? "100%" : "240px");
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
       setTimeout(() => {
         setIsResetting(false);
       }, 300);
@@ -120,7 +120,7 @@ export const Navigation = () => {
         className={cn(
           "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
           isResetting && "transition-all ease-in-out duration-300",
-          isModile && "w-0"
+          isMobile && "w-0"
         )}
         ref={sidebarRef}>
         <div
@@ -128,7 +128,7 @@ export const Navigation = () => {
           onClick={collapse}
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
-            isModile && "opacity-100"
+            isMobile && "opacity-100"
           )}>
           <ChevronLeftIcon className="h-6 w-6" />
         </div>
@@ -152,7 +152,7 @@ export const Navigation = () => {
         className={cn(
           "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
-          isModile && "w-full left-0"
+          isMobile && "w-full left-0"
         )}>
         <nav className="bg-transparent px-3 py-2 w-full">
           {isCollapsed && (
