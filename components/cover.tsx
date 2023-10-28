b35c0d263662726c54f2e3e933eb6c6f90ccfd38
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-10-28 10:28:54 +0530

    better delete and replace functionality

--- components/cover.tsx
+++ components/cover.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import { ImageIcon, X } from "lucide-react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverProps {
   url?: string;
@@ -18,10 +19,16 @@ interface CoverProps {
 
 export const Cover = ({ url, preview }: CoverProps) => {
   const params = useParams();
+  const { edgestore } = useEdgeStore();
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.remmoveCover);
 
   const onRemove = async () => {
+    if(url ){
+     await edgestore.publicFiles.delete({
+        url: url as string,
+      });
+    }
     removeCoverImage({
       id: params.documentId as Id<"documents">,
     });
@@ -38,7 +45,7 @@ export const Cover = ({ url, preview }: CoverProps) => {
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() =>coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm">
