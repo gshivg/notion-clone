c7ba3363fc50d93f436ea36dc268ad743411b9f0
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-10-28 11:22:08 +0530

    redirect on new note

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -11,7 +11,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useSearch } from "@/hooks/use-search";
 import { useMediaQuery } from "usehooks-ts";
@@ -31,6 +31,7 @@ import { useSettings } from "@/hooks/use-settings";
 import { NavBar } from "./navbar";
 
 export const Navigation = () => {
+  const router = useRouter();
   const search = useSearch();
   const settings = useSettings();
   const pathName = usePathname();
@@ -125,7 +126,9 @@ export const Navigation = () => {
   };
 
   const handleCreate = () => {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" }).then((documentId) => {
+      router.push(`/documents/${documentId}`);
+    });
 
     toast.promise(promise, {
       loading: "Creating document...",
