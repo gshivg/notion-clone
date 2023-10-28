ae0d4326c1f8b69ade948d48a6d6915ae8606019
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-10-28 12:47:02 +0530

    bg fix

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,13 +4,13 @@ import Footer from "./_components/footer";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-[#1f1f1f]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        <Heading /> 
+        <Heading />
         <Heroes />
       </div>
-      <Footer /> 
+      <Footer />
     </div>
   );
-}
+};
 export default MarketingPage;
